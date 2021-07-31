2889fc0d52c6defcf027c802312ccf9a78dfece3
Rocky <lixiangwen215@163.com>
2021-07-31 19:42:33 +0800

    third commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -7,7 +7,7 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
-//        System.out.println("hello git3!");
+        System.out.println("hello git3!");
 //        System.out.println("hello git4!");
 //        System.out.println("master test!");
 //        System.out.println("hot-fix test!");
